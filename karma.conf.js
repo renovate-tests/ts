dd1c075921b90e56a5d63a5d473f7e81f15a8166
Keisuke Kan <9renpoto@gmail.com>
2015-08-10 20:20:53 +0900

    try webpack

--- karma.conf.js
+++ karma.conf.js
@@ -1,70 +1,46 @@
-// Karma configuration
-// Generated on Mon Mar 23 2015 17:48:11 GMT+0900 (JST)
+var webpack = require('webpack');
+var path = require('path');
 
 module.exports = function(config) {
   config.set({
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'requirejs'],
-
-
-    // list of files / patterns to load in the browser
+    frameworks: ['mocha'],
     files: [
-      'test/js/require.config.js',
-      {pattern: 'bower_components/underscore/underscore.js', included: false},
-      {pattern: 'bower_components/power-assert/build/power-assert.js', included: false},
-      {pattern: 'src/js/*.js', included: false},
-      {pattern: 'test/*/*Spec.js', included: false}
+      'test/test.index.js',
     ],
-
-
-    // list of files to exclude
     exclude: [
       'src/require.config.js'
     ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'test/test.index.js': ['webpack'],
     },
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['dots'],
-
-
-    // web server port
+    reporters: ['progress'],
     port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['PhantomJS'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true
+    singleRun: true,
+    webpack: {
+      module: {
+        loaders: [
+          { test: /_test\.js$/, exclude: /node_modules/, loader: 'webpack-espower-loader' }
+        ]
+      },
+      resolve: {
+        root: path.join(__dirname, 'src'),
+        modulesDirectories: [
+          'node_modules',
+          'bower_components',
+        ],
+        extensions: ['', '.coffee', '.js']
+      },
+      plugins: [
+        new webpack.ResolverPlugin([
+          new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin(
+            ['bower.json'], ['main'])
+        ])
+      ]
+    },
   });
 };
---
+++ test/foo_test.js
@@ -0,0 +1,10 @@
+define(function(require) {
+  var assert = require('power-assert');
+
+  describe('sample', function(){
+    it('sample', function(){
+      assert.ok(true);
+    });
+  });
+
+});
