b868df5c30bb3ac900a57ff85ad53d73c7222bd3
Keisuke Kan <9renpoto@gmail.com>
2015-09-04 08:09:44 +0900

    add sinon

--- test/foo_test.js
+++ test/foo_test.js
@@ -1,12 +1,10 @@
-define(function(require) {
-  var assert =require('power-assert');
-  var _ = require('underscore');
+var assert =require('power-assert');
+var _ = require('underscore');
+var sinon = require('sinon');
 
-  describe('sample', function(){
-    it('sample', function(){
-      assert.ok(true);
-      assert.ok(_.isFunction(_));
-    });
+describe('sample', function(){
+  it('sample', function(){
+    assert.ok(true);
+    assert.ok(_.isFunction(_));
   });
-
 });
