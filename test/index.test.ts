7bc1b1654b912bc693beea319e1b2d9b6c909a76
Keisuke Kan <9renpoto@gmail.com>
2016-11-27 08:25:25 +0900

    fix(test) fix lint

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,17 +1,18 @@
+import 'mocha'
 import Calculator from '../src'
 import * as assert from 'power-assert'
 
 describe('Calculator', () => {
   let subject: Calculator
 
-  beforeEach(function() {
+  beforeEach(() => {
     subject = new Calculator()
   })
 
   describe('#add', () => {
     it('should add two numbers together', () => {
-      let result: number = subject.add(2, 3)
-      assert(5 === result)
+      const result: number = subject.add(2, 3)
+      assert(result === 5)
     })
   })
 })
