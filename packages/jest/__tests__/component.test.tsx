346b703e4f2617a41842e07f309a7909a0ab6b21
Keisuke Kan <9renpoto@gmail.com>
2017-10-03 22:10:41 +0900

    feat(packages/jest) add inferno sample, wip

---
+++ packages/jest/__tests__/component.test.tsx
@@ -0,0 +1,36 @@
+import Inferno from 'inferno'
+import Component from 'inferno-component'
+import createClass from 'inferno-create-class'
+import createElement from 'inferno-create-element'
+import TestUtils from 'inferno-test-utils'
+
+const FunctionalComponent = function (props) {
+  return createElement('div', props)
+}
+
+xdescribe('renderToSnapshot', () => {
+  it('should return a snapshot from a valid vNode', () => {
+    const snapshot = TestUtils.renderToSnapshot(
+      <FunctionalComponent foo='bar' />
+    )
+
+    if (usingJest) {
+      expect(snapshot).toMatchSnapshot()
+    } else {
+      expect(snapshot.props.foo).toBeDefined()
+      expect(snapshot.props.foo).toBe('bar')
+    }
+  })
+
+  it('should return a snapshot with className prop', () => {
+    const TestComponent = () => <div className='test'>Test</div>
+
+    const snapshot = TestUtils.renderToSnapshot(<TestComponent />)
+
+    if (usingJest) {
+      expect(snapshot).toMatchSnapshot()
+    } else {
+      expect(snapshot.props.className).toBe('test')
+    }
+  })
+})
